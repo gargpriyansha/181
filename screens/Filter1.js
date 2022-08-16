8a6b3118a310ab7db950b2e27ac7545e855773dc
gargpriyansha <62380371+gargpriyansha@users.noreply.github.com>
2022-08-17 01:59:35 +0530

    Add files via upload

---
+++ screens/Filter1.js
@@ -0,0 +1,36 @@
+import React from "react" ;
+import {Image, View} from 'react-native' ; 
+
+const Filter1 =({
+    face:{
+        bounds:{
+            size:{width : faceWidth, height:faceHeight}
+        },
+        leftEyePosition, 
+        rightEyePosition
+    }
+ }) =>{
+    const glassesWidth = FaceWidth 
+    const glassesHeight = faceHeight/3
+    const transformAngle =(
+     angleRad = Math.atan ((rightEyePosition.y - leftEyePosition.y)/ (rightEyePosition.x-leftEyePosition.x))
+    ) =>angleRad * 180/Math.PI 
+return (
+    <View style ={{
+        position:'absolute', 
+        left : leftEyePosition.x - glassesWidth *0.675,
+        right : leftEyePosition.y - glassesHeight *0.5 
+    }}>
+    <Image 
+    source ={require('../assets/glasses.png')}
+    style={{
+        width:glassesWidth, 
+        height:glassesHeight, 
+        resizeMode:'contain', 
+        transform:[{rotate: `${transformAngle()}deg`}]
+    }} />
+
+    </View>
+)
+}
+export default Filter1; 
